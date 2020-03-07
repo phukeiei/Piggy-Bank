0834349ff5307a2d57e02c9450b11e8568c8d69c
Lalitaies <54170254+Lalitaies@users.noreply.github.com>
2020-03-07 16:18:46 +0700

    เขียนโค้ด ExpenseCategory

--- Frontend/src/pages/ExpenseCategory.vue
+++ Frontend/src/pages/ExpenseCategory.vue
@@ -1,5 +1,40 @@
 <template>
   <q-page padding>
+    <div class="q-pa-md row items-start q-gutter-md">
+      <q-card class="my-card col-11">
+        <q-card-section align="left" class="bg-primary text-white">
+          <div class="q-gutter-sm">
+            <q-btn color="secondary" label="+" />
+          </div>
+        </q-card-section>
+
+        <div class="q-pa-md row items-start q-gutter-md">
+          <q-card class="my-card" style="width: 100%">
+            <q-card-section>
+              <div class="row items-center justify-between">
+                <div style="margin:20px">(ตัวอย่างรูป)</div>
+                <q-card-section>เงินเดือน</q-card-section>
+                <div class="q-pa-md q-gutter-sm">
+                  <q-btn color="secondary" label="-" />
+                </div>
+              </div>
+            </q-card-section>
+          </q-card>
+        </div>
+
+        <div class="q-pa-md row items-start q-gutter-md">
+          <q-card class="my-card" style="width: 100%">
+            <q-card-section>
+              <div class="row items-center justify-between">
+                <div style="margin:20px">(ตัวอย่างรูป)</div>
+                <q-card-section>โบนัส</q-card-section>
+                <div class="q-pa-md q-gutter-sm">
+                  <q-btn color="secondary" label="-" />
+                </div>
+              </div>
+            </q-card-section>
+          </q-card>
+        </div>
     <q-footer elevated style="background-color:teal">
       <q-tabs v-model="tab" inline-label class="bg-primary text-white shadow-2">
         <q-route-tab name="mails" icon="mail" label="รายจ่าย" to="/expenseCategory" exact />
