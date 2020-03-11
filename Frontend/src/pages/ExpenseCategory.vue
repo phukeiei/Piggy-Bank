55df19aa05f70c7f6e033fab912f3ed29fb84390
Thimaphon Niramitwasu <60160338@go.buu.ac.th>
2020-03-11 09:23:35 +0700

    Update ExpenseCategory.vue

--- Frontend/src/pages/ExpenseCategory.vue
+++ Frontend/src/pages/ExpenseCategory.vue
@@ -4,7 +4,39 @@
       <q-card flat class="my-card">
         <q-card-section align="left" class="bg-teal text-white">
           <div class="q-gutter-sm">
-            <q-btn color="teal-5" icon="add" />
+            <q-btn color="teal-5" icon="add"  @click="prompt = true"/>
+             <q-dialog v-model="prompt" persistent full-width>
+        <q-card class="column items-center justify-around">
+          <q-card-section>
+            <div class="text-h6">เพิ่มหมวดหมู่</div>
+          </q-card-section>
+
+          <div class="q-pa-md q-gutter-sm">
+            <q-avatar
+              size="100px"
+              font-size="52px"
+              color="teal"
+              text-color="white"
+              icon="directions"
+            />
+          </div>
+
+          <q-card-section class="q-pt-none">
+            <q-input
+              dense
+              v-model="address"
+              autofocus
+              @keyup.enter="prompt = false"
+              placeholder="กรุณากรอกชื่อหมวดหมู่"
+            />
+          </q-card-section>
+
+          <q-card-actions align="right" class="text-primary">
+            <q-btn outline color="red" label="ยกเลิก" v-close-popup />
+            <q-btn outline color="green" label="ตกลง" v-close-popup />
+          </q-card-actions>
+        </q-card>
+      </q-dialog>
           </div>
         </q-card-section>
 
@@ -24,6 +56,7 @@
                   <q-btn color="secondary" icon="remove" @click="dialog = true" />
                 </q-item-label>
               </q-item-section>
+              
             </div>
           </q-card>
         </div>
@@ -42,13 +75,34 @@
               <q-item-section>
                 <q-item-label class="text-center">
                   <q-btn color="secondary" icon="remove" @click="dialog = true" />
+                  <q-dialog v-model="dialog">
+              <q-card style="width: 300px">
+                <q-card-section>
+                  <div class="text-h7">ต้องการลบ "รายการ"?</div>
+                </q-card-section>
+                <!-- <q-card-section class="row items-center q-gutter-sm"> -->
+                <q-card-section>
+                  <div class="row">
+                    <div class="col">
+                      <q-btn outline class="text-right" color="red" label="ยกเลิก" v-close-popup />
+                    </div>
+                    <div class="col" align="right">
+                      <q-btn outline class="text-left" color="green" label="ตกลง" v-close-popup />
+                    </div>
+                  </div>
+                </q-card-section>
+              </q-card>
+            </q-dialog>
                 </q-item-label>
+                
               </q-item-section>
             </div>
+            
           </q-card>
         </div>
       </q-card>
     </div>
+    
 
     <q-footer elevated style="background-color:teal">
       <q-tabs v-model="tab" inline-label class="bg-primary text-white shadow-2">
@@ -68,7 +122,16 @@
 
 <script>
 export default {
-  // name: 'PageName',
+  data() {
+    return {
+      alert: false,
+      confirm: false,
+      prompt: false,
+      dialog: false,
+
+      address: ""
+    };
+  }
 };
 </script>
 
